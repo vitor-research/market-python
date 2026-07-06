b6456ed437261e418437fb5b6336e3dac2478084
verificaicode <oiiaiiocat@gmail.com>
2026-07-06 20:55:51 -0300

    update

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def scan_opportunities(threshold_param):
             
             curr_mid = df['mid'].iloc[-1]
             curr_atr = df['atr'].iloc[-1]
-            atr_pct = curr_atr/curr_mid * 10
+            atr_pct = curr_atr/curr_mid * 5
             features = df[['mom', 'atr', 'vol_rel']].iloc[[-1]].values.astype(np.float32)
             
             # Obter a sessão ONNX da moeda
@@ -89,7 +89,7 @@ def scan_opportunities(threshold_param):
                     "is_buy": True,
                     "prob": probs[2],
                     "tp": round(1 + (atr_pct * 3.0), 6),
-                    "sl": round(1 - (atr_pct * 1.5), 6),
+                    "sl": round(1 - (atr_pct * 2.0), 6),
                     "leverage": 4
                 })
             elif probs[0] > threshold_param:
